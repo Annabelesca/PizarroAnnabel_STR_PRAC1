58073e6b5386f11924c134f82a00f647a89a0ae5
Ann <37332222+Annabelesca@users.noreply.github.com>
2020-12-17 17:13:46 +0100

    Add files via upload
    Añadida la funcionalidad de regular la cantidad de luz a compensar con un potenciometro.

--- main.cpp
+++ main.cpp
@@ -3,59 +3,133 @@
 #include "rtos.h"
 
 
+//Definimos las constantes que nos permitiran calcular la lux
 #define luxRel 500
 #define VRef 3.3
 #define Rl 10
 #define FrecMuestreo 100
+#define FrecPotenciometro 1s
 
+//Definimos los sensores/actuadores y los pins donde se ecuentran
 PwmOut Led(D5);
 AnalogIn Photoresistor(A0);
 AnalogIn Potentiometer(A2);
 InterruptIn AvButton(A3);
 Thread AvCalculator(osPriorityAboveNormal);
-osThreadId ISRthreadId;
+osThreadId AvCalculatorID;
 
-void AvCalc();
-void AvCalculator_Thread();
+Mutex stdio_mutex;
 
-float luxCalc(float lectura){
-    float Vout = lectura*(VRef/1);
-    return (((VRef*luxRel)*Vout)-luxRel)/Rl;
+Thread LinPotenciometer(osPriorityAboveNormal);
+osThreadId LinPotID;
+Ticker potInterrupt;
+
+//Variables globales
+float lindar = 1;       //Definirá hasta que porcentaje de luz, compensará el LED
+
+void AvCalc_Interrupcion();
+void AvCalc_Thread();
+void LinPot_Interrupcion();
+void LinPot_Thread();
+
+/*
+luxCalc
+    Funcion auxiliar que se encarga de calcular el porcentaje de luz que debe compensar el LED así como también el nivel de luminosidad que hay en el ambiente
+Parametros:
+    datos: Array de tres posiciones que contiene el porcentaje de luz que capta el sensor en la posicion 1
+Retorna:
+    datos: Array con las posiciones 0 y 2 modificadas. La posicion 0 contendrá el nivel de luminosidad (lux) y la posicion 2 el % de compensacion del LED
+*/
+void luxCalc(float datos[3]){
+    float vout = datos[1]*(VRef/1);
+    datos[0] = (((VRef*luxRel)*vout)-luxRel)/Rl;
+    stdio_mutex.lock(); 
+    if (lindar-datos[1]<0) datos[2]=0;
+    else datos[2] = lindar - datos[1];
+    stdio_mutex.unlock();
 }
 
+/*
+    Programa principal que se encarga de leer la luz ambiental y compensar con un led esta cantidad de luz
+    Funcionalidades extra:
+        - Muestra periodicamente por pantalla la luz (capturada y compensada por LED) en forma de porcentajes.
+        - Detecta errores y activa un buzzer, indicando el error.
+        - Permite hacer la media de los datos captados durante 10 segundos aprentando un boton. Una vez pasados los diez segundos, 
+        se muestran los datos medios.
+        - Permite escoger la cantidad de luz a compensar mediante un potenciometro.
+*/
 int main()
 {
-    AvCalculator.start(callback(AvCalculator_Thread));    //Iniciamos thread
-    AvButton.rise(&AvCalc);                               //
+    float datos[3] = {0, 0, 0};
+    AvCalculator.start(callback(AvCalc_Thread));    //Iniciamos thread
+    LinPotenciometer.start(callback(LinPot_Thread));
+    AvButton.rise(&AvCalc_Interrupcion);    
+    potInterrupt.attach(&LinPot_Interrupcion, FrecPotenciometro);                           
 
     printf("\n\n======== STR Practica 1: Compensador de llum ========\n\n");
     while (true) {
-        float porcLuz = Photoresistor.read();   //Leemos % de luz en el ambiente
-        float lux = luxCalc(porcLuz);           //Calculamos nivel de luminancia
-        float porcLed = 1-porcLuz;              //Calculamos % de luz que debe compensar el LED
-        Led=porcLed;                            //Encendemos LED
-        printf("Nivel de iluminancia: %.2f\t\tLuz ambiente: %.2f \t\tCompensación led: %.2f \n", lux, porcLuz*100, porcLed*100);
-        wait_us(1000000);                       //Esperamos 1 segundo entre lecturas
+        datos[1] = Photoresistor.read();   //Leemos % de luz en el ambiente
+        luxCalc(datos);                    //Calculamos nivel de luminancia y % de compensacion por el LED
+        Led.write(datos[2]);
+        printf("Nivel de iluminancia (lux): %.2f \t\tLuz ambiente: %.2f %% \t\tCompensación led: %.2f %% ", datos[0], datos[1]*100, datos[2]*100);
+        printf("\tCantidad de luz a compensar: %f %%\n", lindar*100);
+        wait_us(1500000);                       //Esperamos 1.5 segundos entre lecturas
     }
 }
 
-void AvCalc(){
-   osSignalSet(ISRthreadId, 0x01);
+/*
+AvCalc_Interrupcion
+    Interrupcion que se encarga de despertar el thread encargado de hacer la media de datos
+*/
+void AvCalc_Interrupcion(){
+   osSignalSet(AvCalculatorID, 0x01);
 }
 
-void AvCalculator_Thread(){
-    ISRthreadId = osThreadGetId();
-    float average;
+/*
+AvCalc_Thread
+    Funcion que se ejecuta como thread. Se encarga de calcular la media de los datos durante 10 segundos, usando para ello un muestreo de 100 datos
+*/
+void AvCalc_Thread(){
+    AvCalculatorID = osThreadGetId();
+    float average, lectura, datos[3];
     for(;;){
         osSignalWait(0x01, osWaitForever);
-        printf("\nComenzamos a calcular la media de los siguientes 10 segundos (%u muestras)", FrecMuestreo);
+        potInterrupt.detach();
+        printf("\nComenzamos a calcular la media de los siguientes 10 segundos (%u muestras)\n", FrecMuestreo);
         average=0;
         for(int i=0; i<FrecMuestreo; i++){
-            average=average+Photoresistor.read();
+            lectura=Photoresistor.read();
+            average=average+lectura;
             wait_us((10/(float)FrecMuestreo)*1000000);
         }
-        average=average/FrecMuestreo;
+        datos[1]=average/FrecMuestreo;
         printf("\nRegistro de la media de los datos en los últimos 10 segundos:\n");
-        printf("\tNivel de iluminancia: %.2f\t\tLuz ambiente: %.2f \t\tCompensación led: %.2f \n\n", luxCalc(average), average*100, (1-average)*100);       
+        luxCalc(datos);
+        printf("\tNivel de iluminancia (lux): %.2f\t\tLuz ambiente: %.2f %% \t\tCompensación led: %.2f %% \n\n", datos[0], datos[1]*100, datos[2]*100);
+        potInterrupt.attach(&LinPot_Interrupcion, FrecPotenciometro);       
+    }
+}
+
+/*
+    LinPot_Interrupcion
+    Interrupcion que se encarga de despertar el thread encargado de comprobar el potenciometro
+*/
+
+void LinPot_Interrupcion(){
+    osSignalSet(LinPotID, 0x01);
+}
+
+/*
+  LinPot_Thread
+  Funcion que se encargará de calcular el lindar de luz a compensar en funcion al valor del potenciometro
+  Se actualizará en funcion de la constante FrecPotenciometro  
+ */
+void LinPot_Thread(){
+    LinPotID = osThreadGetId();
+    for(;;){
+        osSignalWait(0x01, osWaitForever);
+        stdio_mutex.lock();
+        lindar = Potentiometer.read();
+        stdio_mutex.unlock();
     }
 }
