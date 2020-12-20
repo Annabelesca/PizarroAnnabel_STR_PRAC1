72d762e2fa1ffe5368e3be4497b96de4730004fc
Ann <37332222+Annabelesca@users.noreply.github.com>
2020-12-20 13:18:51 +0100

    Add files via upload
    Añadida la funcionalidad de detectar errores en la conexión de los pines de los sensores.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include <cstdio>
+#include <cstdlib>
 #include "rtos.h"
 
 
@@ -8,7 +9,9 @@
 #define VRef 3.3
 #define Rl 10
 #define FrecMuestreo 100
-#define FrecPotenciometro 1s
+#define FrecPotenciometro 200ms
+#define MuestrasError 10
+#define Desviacion 0.025
 
 //Definimos los sensores/actuadores y los pins donde se ecuentran
 PwmOut Led(D5);
@@ -22,15 +25,17 @@ Mutex stdio_mutex;
 
 Thread LinPotenciometer(osPriorityAboveNormal);
 osThreadId LinPotID;
-Ticker potInterrupt;
+Ticker timer;
 
 //Variables globales
 float lindar = 1;       //Definirá hasta que porcentaje de luz, compensará el LED
+char hayError = 0;  //Variable que indicará si existe algún error en el programa
 
 void AvCalc_Interrupcion();
 void AvCalc_Thread();
 void LinPot_Interrupcion();
 void LinPot_Thread();
+
 
 /*
 luxCalc
@@ -50,6 +55,30 @@ void luxCalc(float datos[3]){
 }
 
 /*
+comprobarFuncionamientoSensor
+    Funcon auxiliar que se encarga de controlar el correcto funcionamiento del sensor que se le pasa por parametro haciendo una media de valores leidos y mirando
+    la desviacion
+Parametros:
+    sensor: Sensor a ser comprobado
+Retorna:
+    false si no hay error y true si lo hay
+*/
+bool comprobarFuncionamientoSensor(AnalogIn sensor){
+    float average=0, lectura; 
+    bool error=false;
+
+    for (int i=0; i<MuestrasError; i++) {   
+        lectura=sensor.read();
+        average=average+lectura;
+    }
+    average=average/MuestrasError;
+    if (abs(average-lectura)>=Desviacion) error=true; //Si diferencia entre la media y la ultima lectura no está comprendida en un rango marcado por la desviacion,
+    //es que puede existir un posible error
+    
+    return error;
+}
+
+/*
     Programa principal que se encarga de leer la luz ambiental y compensar con un led esta cantidad de luz
     Funcionalidades extra:
         - Muestra periodicamente por pantalla la luz (capturada y compensada por LED) en forma de porcentajes.
@@ -60,20 +89,25 @@ void luxCalc(float datos[3]){
 */
 int main()
 {
+    printf("\n\n============================ STR Practica 1: Compensador de llum ============================\n\n");
+
     float datos[3] = {0, 0, 0};
     AvCalculator.start(callback(AvCalc_Thread));    //Iniciamos thread
     LinPotenciometer.start(callback(LinPot_Thread));
-    AvButton.rise(&AvCalc_Interrupcion);    
-    potInterrupt.attach(&LinPot_Interrupcion, FrecPotenciometro);                           
+    AvButton.rise(&AvCalc_Interrupcion);
+    timer.attach(&LinPot_Interrupcion, FrecPotenciometro);
 
-    printf("\n\n======== STR Practica 1: Compensador de llum ========\n\n");
     while (true) {
+        if (comprobarFuncionamientoSensor(Photoresistor)==true){
+            printf("Error en el funcionamiento del fotoresistor. Revisa si el pin esta conectado");
+            exit(0);
+        }
         datos[1] = Photoresistor.read();   //Leemos % de luz en el ambiente
         luxCalc(datos);                    //Calculamos nivel de luminancia y % de compensacion por el LED
         Led.write(datos[2]);
         printf("Nivel de iluminancia (lux): %.2f \t\tLuz ambiente: %.2f %% \t\tCompensación led: %.2f %% ", datos[0], datos[1]*100, datos[2]*100);
         printf("\tCantidad de luz a compensar: %f %%\n", lindar*100);
-        wait_us(1500000);                       //Esperamos 1.5 segundos entre lecturas
+        ThisThread::sleep_for(1s);                       //Esperamos 1 segundos entre lecturas
     }
 }
 
@@ -94,7 +128,7 @@ void AvCalc_Thread(){
     float average, lectura, datos[3];
     for(;;){
         osSignalWait(0x01, osWaitForever);
-        potInterrupt.detach();
+        timer.detach();
         printf("\nComenzamos a calcular la media de los siguientes 10 segundos (%u muestras)\n", FrecMuestreo);
         average=0;
         for(int i=0; i<FrecMuestreo; i++){
@@ -106,7 +140,7 @@ void AvCalc_Thread(){
         printf("\nRegistro de la media de los datos en los últimos 10 segundos:\n");
         luxCalc(datos);
         printf("\tNivel de iluminancia (lux): %.2f\t\tLuz ambiente: %.2f %% \t\tCompensación led: %.2f %% \n\n", datos[0], datos[1]*100, datos[2]*100);
-        potInterrupt.attach(&LinPot_Interrupcion, FrecPotenciometro);       
+        timer.attach(&LinPot_Interrupcion, FrecPotenciometro);
     }
 }
 
@@ -114,7 +148,6 @@ void AvCalc_Thread(){
     LinPot_Interrupcion
     Interrupcion que se encarga de despertar el thread encargado de comprobar el potenciometro
 */
-
 void LinPot_Interrupcion(){
     osSignalSet(LinPotID, 0x01);
 }
@@ -127,9 +160,14 @@ void LinPot_Interrupcion(){
 void LinPot_Thread(){
     LinPotID = osThreadGetId();
     for(;;){
-        osSignalWait(0x01, osWaitForever);
+        if (comprobarFuncionamientoSensor(Potentiometer)==true){
+            printf("Error en el funcionamiento del potenciometro. Revisa si el pin esta conectado");
+            exit(0);
+        }
+        //printf("\t***log: Comprobamos valor del potenciometro\n");
         stdio_mutex.lock();
         lindar = Potentiometer.read();
-        stdio_mutex.unlock();
+        stdio_mutex.unlock();   
+        osSignalWait(0x01, osWaitForever);  //Wait al final de la iteracion para hacer la comprobación del funcionamiento nada mas empezar el thread   
     }
 }
